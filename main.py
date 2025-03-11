0355779baa1f651a9d4f5d45766386cdc855df10
augusto-ludewig <augustoludewig16@gmail.com>
2025-03-11 18:42:16 -0300

    Ajuste na estrutura dos laços de repetição

--- main.py
+++ main.py
@@ -18,8 +18,10 @@ while True:
     print('(4) Gerenciar turmas')
     print('(5) Gerenciar matriculas')
     print('(9) Sair')
-    categoria = str(input('\nInforme a opção desejada: '))
+
+    categoria = str(input('\nInforme a opção desejada: ')) #Entrada de dados para seleção
     
+    # Conversão da seleção em palavra para ser usado como variável
     if (categoria == '1'):
         categoria = 'ESTUDANTES'
     elif (categoria == '2'):
@@ -35,72 +37,71 @@ while True:
         break
     else:
         print('\nOpção inválida. Tente novamente.\n')
-    break
-
-# Sub menu - Funcionalidades
-while True:
-    print('\n***** [' + categoria + '] MENU DE OPERAÇÕES *****')
-    print('(1) Incluir')
-    print('(2) Listar')
-    print('(3) Excluir')
-    print('(4) Alterar')
-    print('(9) Voltar ao menu principal')
-    funcionalidade = input('\nInforma a ação desejada: ').lower()
-    if funcionalidade in ['1', '2', '3', '4', 'incluir', 'listar', 'excluir', 'alterar']:
-        match funcionalidade:  
-        #INCLUIR
-            case '1':
-                if(categoria == 'ESTUDANTES'):
-                    listaAlunos.append(input('Nome do aluno: '))
-                elif(categoria == 'DISCIPLINAS'):
-                    listaDisciplinas.append(input('Nome da disciplina: '))
-                elif(categoria == 'PROFESSORES'):
-                    listaProfessores.append(input('Nome do professor: '))
-                elif(categoria == 'TURMAS'):
-                    listaTurmas.append(input('Nome da turma: '))
-                elif(categoria == 'MATRICULAS'):
-                    listaMatricula.append(input('Numero da matrícula: '))
-            #LISTAR
-            case '2': 
-                if(categoria == 'ESTUDANTES'):
-                    print('\nAlunos: ')
-                    for nome in listaAlunos:
-                        print('- ' + nome)
-                elif(categoria == 'DISCIPLINAS'):
-                    print('\nDisciplinas: ')
-                    for nome in listaDisciplinas:
-                        print('- ' + nome)
-                elif(categoria == 'PROFESSORES'):
-                    print('\nProfessores: ')
-                    for nome in listaProfessores:
-                        print('- ' + nome)
-                elif(categoria == 'TURMAS'):
-                    print('\nTurmas: ')
-                    for nome in listaTurmas:
-                        print('- ' + nome)
-                elif(categoria == 'MATRICULAS'):
-                    print('\nMatrículas: ')
-                    for nome in listaMatricula:
-                        print('- ' + nome)
-            #EXCLUIR
-            case '3':
-                try:
+    
+    while True and categoria in ['ESTUDANTES', 'DISCIPLINAS', 'PROFESSORES', 'TURMAS', 'MATRICULAS']:
+        # Sub menu - Funcionalidades
+        print('\n***** [' + categoria + '] MENU DE OPERAÇÕES *****')
+        print('(1) Incluir')
+        print('(2) Listar')
+        print('(3) Excluir')
+        print('(4) Alterar')
+        print('(9) Voltar ao menu principal')
+        funcionalidade = input('\nInforma a ação desejada: ').lower()
+        if funcionalidade in ['1', '2', '3', '4', 'incluir', 'listar', 'excluir', 'alterar']:
+            match funcionalidade:  
+            #INCLUIR
+                case '1':
                     if(categoria == 'ESTUDANTES'):
-                        listaAlunos.remove(input('Digite o nome do aluno para ser removido: '))
+                        listaAlunos.append(input('Nome do aluno: '))
                     elif(categoria == 'DISCIPLINAS'):
-                        listaDisciplinas.remove(input('Digite o nome do aluno para ser removido: '))
+                        listaDisciplinas.append(input('Nome da disciplina: '))
                     elif(categoria == 'PROFESSORES'):
-                        listaProfessores.remove(input('Digite o nome do aluno para ser removido: '))
+                        listaProfessores.append(input('Nome do professor: '))
                     elif(categoria == 'TURMAS'):
-                        listaTurmas.remove(input('Digite o nome do aluno para ser removido: '))
+                        listaTurmas.append(input('Nome da turma: '))
                     elif(categoria == 'MATRICULAS'):
-                        listaMatricula.remove(input('Digite o nome do aluno para ser removido: '))
-                except:
-                    print('Erro ao excluir! ' + categoria + ' não registrado.')
-            #ALTERAR
-            case '4':
-                print('Selecionou d')
-    elif(funcionalidade == '9'):
-        break
-    else:
-        print('Opção inválida. Tente novamente.')
+                        listaMatricula.append(input('Numero da matrícula: '))
+                #LISTAR
+                case '2': 
+                    if(categoria == 'ESTUDANTES'):
+                        print('\nAlunos: ')
+                        for nome in listaAlunos:
+                            print('- ' + nome)
+                    elif(categoria == 'DISCIPLINAS'):
+                        print('\nDisciplinas: ')
+                        for nome in listaDisciplinas:
+                            print('- ' + nome)
+                    elif(categoria == 'PROFESSORES'):
+                        print('\nProfessores: ')
+                        for nome in listaProfessores:
+                            print('- ' + nome)
+                    elif(categoria == 'TURMAS'):
+                        print('\nTurmas: ')
+                        for nome in listaTurmas:
+                            print('- ' + nome)
+                    elif(categoria == 'MATRICULAS'):
+                        print('\nMatrículas: ')
+                        for nome in listaMatricula:
+                            print('- ' + nome)
+                #EXCLUIR
+                case '3':
+                    try:
+                        if(categoria == 'ESTUDANTES'):
+                            listaAlunos.remove(input('Digite o nome do aluno para ser removido: '))
+                        elif(categoria == 'DISCIPLINAS'):
+                            listaDisciplinas.remove(input('Digite o nome do aluno para ser removido: '))
+                        elif(categoria == 'PROFESSORES'):
+                            listaProfessores.remove(input('Digite o nome do aluno para ser removido: '))
+                        elif(categoria == 'TURMAS'):
+                            listaTurmas.remove(input('Digite o nome do aluno para ser removido: '))
+                        elif(categoria == 'MATRICULAS'):
+                            listaMatricula.remove(input('Digite o nome do aluno para ser removido: '))
+                    except:
+                        print('Erro ao excluir! ' + categoria + ' não registrado.')
+                #ALTERAR
+                case '4':
+                    print('Selecionou d')
+        elif(funcionalidade == '9' or funcionalidade == 'Voltar ao menu principal'):
+            break
+        else:
+            print('Opção inválida. Tente novamente.')
